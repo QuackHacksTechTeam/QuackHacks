fa53c5a462378aa7c1f3cbeece840db63192068b
Hai <114781373+Doeroy@users.noreply.github.com>
2025-04-24 00:29:17 -0700

    Goodbye my love </3

--- main.js
+++ main.js
@@ -1,3 +1,43 @@
-  setTimeout(() => {
-    document.querySelector('.terminal').classList.add('typed-done');
-  }, 3000); // match duration of typing animation
+const chars = "!<>-_\\/[]{}—=+*^?#________";
+let bool = false;
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll('.scramble').forEach(el => {
+    const originalText = el.dataset.text;
+
+    el.addEventListener('mouseenter', () => {
+      let iterations = 0;
+      const scrambleInterval = setInterval(() => {
+        const scrambled = originalText
+          .split("")
+          .map((char, i) => {
+            if (i < iterations) return originalText[i];
+            return chars[Math.floor(Math.random() * chars.length)];
+          })
+          .join("");
+
+        el.textContent = scrambled;
+
+        if (iterations >= originalText.length) clearInterval(scrambleInterval);
+        iterations += 1 / 2;
+      }, 30);
+    });
+  });
+});
+
+function questionOpen(){
+  const questions = document.querySelector(".questions");
+  let answer;
+  questions.addEventListener("click", () =>{
+    if(!bool){ 
+      bool = true;
+      answer = document.createElement("p");
+      answer.textContent = `Yes! Food will be provided for the duration of the event. We will also have swag and prizes! 
+      For non-UO students, we ask that you manage your own housing and transportation`;
+      questions.appendChild(answer);
+    } else{
+      bool = false;
+      questions.remove(answer);
+    }
+    console.log(bool);
+});
+}
